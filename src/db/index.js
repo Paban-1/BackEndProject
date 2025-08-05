5be27086f7e796640683c92fad31aa1a38f1571c
Paban-1 <rpaban89@gmail.com>
2025-08-05 19:03:34 +0530

    frist commit

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose, { mongo } from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connecttionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\n MongoDB connected !! DB HOST: ${connecttionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MONGODB Connection Field", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
